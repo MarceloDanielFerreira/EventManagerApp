5aafa19a34bc8029a108feb1afaaa052b825081a
MarceloDanielFerreira <113811868+MarceloDanielFerreira@users.noreply.github.com>
2024-06-23 01:35:57 -0400

    Init

---
+++ src/main/java/com/project/eventmanagerapp/EventManagerAppApplication.java
@@ -0,0 +1,13 @@
+package com.project.eventmanagerapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventManagerAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventManagerAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/eventmanagerapp/EventManagerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.eventmanagerapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventManagerAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
